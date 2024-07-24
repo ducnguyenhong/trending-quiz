2768cac13aae542cba5ea600fe9a09a9f27ec638
autoclickvn <autoclickvn@gmail.com>
2024-07-25 00:03:54 +0700

    Update UI

--- src/app/@sidebar/page.tsx
+++ src/app/@sidebar/page.tsx
@@ -10,12 +10,27 @@ const Sidebar = () => {
       routes: [
         {
           title: 'Hot trend',
-          icon: '/images/hot-trend.svg',
+          icon: 'https://cdn-icons-png.flaticon.com/128/10473/10473357.png',
           route: '/hot-trend'
         },
         {
           title: 'Quiz tuần này',
           icon: '/images/calendar-outline.svg',
+          route: '/week'
+        }
+      ]
+    },
+    {
+      title: 'Chủ đề',
+      routes: [
+        {
+          title: 'Bóng đá',
+          icon: 'https://cdn-icons-png.flaticon.com/128/1540/1540454.png',
+          route: '/hot-trend3'
+        },
+        {
+          title: 'Lịch ra mắt',
+          icon: 'https://cdn-icons-png.flaticon.com/128/668/668278.png',
           route: '/week'
         }
       ]
@@ -29,7 +44,7 @@ const Sidebar = () => {
           route: '/hot-trend2'
         },
         {
-          title: 'Quiz tuần này',
+          title: 'Lịch ra mắt',
           icon: '/images/calendar-outline.svg',
           route: '/week'
         }
@@ -38,12 +53,15 @@ const Sidebar = () => {
   ];
 
   return (
-    <Box w="280px" px={4} py={6} borderRight="1px solid #e6e6e6" minH="100vh">
-      <Text as="h1" fontWeight={700} fontSize={22}>
-        Trending Quiz
-      </Text>
+    <Box w="350px" px={4} py={6} borderRight="1px solid #e6e6e6" minH="100vh">
+      <Flex align="center" gap={3} pl={1}>
+        <Image src="/images/logo.png" width={45} height={45} alt="logo" />
+        <Text as="h1" fontWeight={700} fontSize={22}>
+          Trending Quiz
+        </Text>
+      </Flex>
 
-      <Flex direction="column" mt={6} gap={10}>
+      <Flex direction="column" mt={10} gap={10}>
         {MENU_LIST.map((menu) => {
           const { title, routes } = menu;
 
@@ -53,7 +71,7 @@ const Sidebar = () => {
                 {title}
               </Text>
 
-              <Flex direction="column" gap={1}>
+              <Flex direction="column">
                 {routes.map((item) => {
                   const { route, title, icon } = item;
                   const isActive = route === '/hot-trend';
